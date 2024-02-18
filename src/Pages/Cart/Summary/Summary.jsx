17eaf80a89a2a7daefdfd8ddd6c7602503defc00
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-02-18 22:08:55 +0530

    ♻️

--- src/Pages/Cart/Summary/Summary.jsx
+++ src/Pages/Cart/Summary/Summary.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useCart } from '../../../context/cart.context'
 
 export default function Summary() {
+    const [subtotal, setSubtotal] = useState(0);
+    const { cart } = useCart();
+    useEffect(() => {
+        let subtotal = 0;
+        cart.map((item) => {
+            subtotal = subtotal + (item.price * item.quantity)
+        })
+        setSubtotal(Number(subtotal.toFixed(2)))
+
+    }, [cart])
+
     return (
-        <Background><h1>Summary</h1></Background>
+        <Background>
+            <h1>Summary</h1>
+            <Container>
+                <div className='total'>
+                    <h2>Subtotal</h2>
+                    <h3>{subtotal}</h3>
+                </div>
+                <div className='total'>
+                    <h2>Quantity</h2>
+                    <h3>123456</h3>
+                </div>
+            </Container>
+        </Background>
     )
 }
 
@@ -22,5 +46,16 @@ const Background = styled.div`
     }
     @media only screen and (max-width: 640px){
         width: calc(100% - 1rem);
+    }
+`
+const Container = styled.div`
+    margin-top: 1rem;
+    .total{
+        margin-top: 1rem;
+        display: flex;
+        justify-content: space-between;
+        h3{
+            font-weight: 100;
+        }
     }
 `
