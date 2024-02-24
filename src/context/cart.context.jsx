15eb316dccae675712f5b6fe583161e634c2afb3
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-02-25 04:24:27 +0530

    k

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -5,6 +5,7 @@ const CartContext = createContext()
 const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
     const [cartUpdate, setCartUpdate] = useState()
+    
 
     useEffect(() => {
         const timeout = setTimeout( ()=>{
