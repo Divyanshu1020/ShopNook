a3ea3b36df6d72a18cec96df10af1512a7c746b0
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-03-15 01:15:07 +0530

    🎨 Improve UI/UX

--- src/Pages/Home/ProductContainer/ProductContainer.jsx
+++ src/Pages/Home/ProductContainer/ProductContainer.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import './ProductContainer.css'
-//* Components
 import { useEffect } from 'react'
+//* Components
 import Product from '../../../Components/ProductCard/Product.jsx'
 import { useProductList } from '../../../context/product.context.jsx'
 import useApi from '../../../util/useApi.jsx'
 import MFilter from '../Filter/MFilter.jsx'
+//*SVGs
+import NoProducts from '../../../Components/UI/NoProducts.jsx'
 
 
 
@@ -43,21 +45,26 @@ export default function ProductContainer() {
       <div className="products-headline">Popular Product</div>
       <MFilter />
       <div className="products-group-conatiner">
-        <div className="products-group">
-          {productList.map((product) => (
-            <Product
-              key={product._id}
-              id={product._id}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-              thumbnail={product.thumbnail} />
-          ))}
-        </div>
-        <div className="loadmore">
-          <button className='loadmore-btn' onClick={() => { loadmoreProductsHandler() }}>Loadmore</button>
-          <div className='separator'></div>
-        </div>
+        {productList.length === 0 ? 
+        (<NoProducts/>)
+         :
+        (<>
+          <div className="products-group">
+            {productList.map((product) => (
+              <Product
+                key={product._id}
+                id={product._id}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                thumbnail={product.thumbnail} />
+            ))}
+          </div>
+          <div className="loadmore">
+            <button className='loadmore-btn' onClick={() => { loadmoreProductsHandler() }}>Loadmore</button>
+            <div className='separator'></div>
+          </div>
+        </>)}
       </div>
     </div>
 
