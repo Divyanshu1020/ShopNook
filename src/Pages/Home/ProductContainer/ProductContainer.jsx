b46bf7b7f09c326cb8a051e2ab1f750d8a35a2be
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-03-15 03:47:02 +0530

    🎨 Improve CSS

--- src/Pages/Home/ProductContainer/ProductContainer.jsx
+++ src/Pages/Home/ProductContainer/ProductContainer.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductContainer.css'
-import { useEffect } from 'react'
 //* Components
+import Loading from '../../../Components/Loading.jsx'
 import Product from '../../../Components/ProductCard/Product.jsx'
 import { useProductList } from '../../../context/product.context.jsx'
 import useApi from '../../../util/useApi.jsx'
 import MFilter from '../Filter/MFilter.jsx'
-//*SVGs
 import NoProducts from '../../../Components/UI/NoProducts.jsx'
 
 
 
 export default function ProductContainer() {
 
-  const { productList, setProductList, setPage, page, query } = useProductList();
   const { fatchAllProducts } = useApi();
+  const { productList, setProductList, setPage, page, query } = useProductList();
+  const [loading, setLoading] = useState(false);
 
   //* Fatch more products
   const loadmoreProductsHandler = async () => {
@@ -29,13 +29,16 @@ export default function ProductContainer() {
   useEffect(() => {
 
     const fetchData = async () => {
+      setLoading(true)
       const response = await fatchAllProducts(1, {});
       console.log(response);
       if (response) {
         setProductList(response);
       }
+      setLoading(false);
     }
-    if(productList.length === 0) {
+
+    if (productList.length === 0) {
       fetchData();
     }
   }, [])
@@ -45,26 +48,33 @@ export default function ProductContainer() {
       <div className="products-headline">Popular Product</div>
       <MFilter />
       <div className="products-group-conatiner">
-        {productList.length === 0 ? 
-        (<NoProducts/>)
-         :
-        (<>
-          <div className="products-group">
-            {productList.map((product) => (
-              <Product
-                key={product._id}
-                id={product._id}
-                title={product.title}
-                description={product.description}
-                price={product.price}
-                thumbnail={product.thumbnail} />
-            ))}
-          </div>
-          <div className="loadmore">
-            <button className='loadmore-btn' onClick={() => { loadmoreProductsHandler() }}>Loadmore</button>
-            <div className='separator'></div>
-          </div>
-        </>)}
+        {loading ?
+          (<Loading />)
+          :
+          (<div className='inside-products-group-conatiner'>
+            { productList.length === 0 ? 
+              (<NoProducts/>)
+              :
+              (<>
+                <div className="products-group">
+                  {productList.map((product) => (
+                    <Product
+                      key={product._id}
+                      id={product._id}
+                      title={product.title}
+                      description={product.description}
+                      price={product.price}
+                      thumbnail={product.thumbnail} />
+                  ))}
+                </div>
+                <div className="loadmore">
+                  <button className='loadmore-btn' onClick={() => { loadmoreProductsHandler() }}>Loadmore</button>
+                  <div className='separator'></div>
+                </div>
+              </>)
+            }
+          </div>)
+        }
       </div>
     </div>
 
