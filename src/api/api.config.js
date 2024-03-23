9c8ed9abb74aa6959e4229a3674ebe7bd9711ba0
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-03-23 20:10:28 +0530

    ⚡️ Improve add api config file

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const BASE_URL = 'http://localhost:8000/api/v1/'
-console.log(import.meta.env.VITE_BASE_URL);
 export default axios.create({
     baseURL : BASE_URL,
     withCredentials : true,
